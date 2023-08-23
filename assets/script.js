44fe01b296f8f726943bda249787c352e53bc71a
THEVENARD Julien <thevenard.julien@gmail.com>
2023-08-23 10:39:59 +0200

    Ajout du carousel

--- assets/script.js
+++ assets/script.js
@@ -8,20 +8,19 @@ const slides = [
     image: "slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
-  // ,
-  // {
-  //   image: "slide2.jpg",
-  //   tagLine:
-  //     "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
-  // },
-  // {
-  //   image: "slide3.jpg",
-  //   tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
-  // },
-  // {
-  //   image: "slide4.png",
-  //   tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
-  // },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
 ];
 
 // Generate slides
@@ -39,20 +38,25 @@ slides.forEach((element) => {
   carousel.appendChild(slide);
 });
 
-// <img
-//   class="banner-img"
-//   src="./assets/images/slideshow/slide1.jpg"
-//   alt="Banner Print-it"
-// />
-// <p>Impressions tous formats <span>en boutique et en ligne</span></p>
-
 // Arrows
+
+const carouselWidth = document.querySelector(".carousel").offsetWidth;
+
+function previous() {
+  carousel.scrollLeft -= carouselWidth;
+}
+
+function next() {
+  carousel.scrollLeft += carouselWidth;
+}
 
 arrowLeft.addEventListener("click", () => {
   console.log("Arrow left");
+  previous();
 });
 arrowRight.addEventListener("click", () => {
-  console.log("Arrow Right");
+  console.log("Arrow right");
+  next();
 });
 
 // Bullet points
